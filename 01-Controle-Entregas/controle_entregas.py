b2803e5b25dfe26479db47eec7bbf6fab69a07b7
WashingtonQuant <primarios108@gmail.com>
2026-06-09 17:33:42 -0300

    Update controle_entregas.py

--- 01-Controle-Entregas/controle_entregas.py
+++ 01-Controle-Entregas/controle_entregas.py
@@ -1,10 +1,27 @@
+import sqlite3
+
 def faturamento_diario():
 
     """O objetivo deste final deste projeto, é fazer as comandas de pedidos, sejam guardadas pelos seus indices, e somada seus valores
-    para resolver o problema de ir somando as comandas na calculadora e em um dado momento, aquela soma sumir e vc perder os valores somados
+    para resolver o problema de ir somando as comandas na calculadora e em um dado momento, aquela soma sumir e você perder os valores somados
     """
-
-    historico_entregas = {"Diária":40}
+    conexao = sqlite3.connect("entregas.db")
+    cursor = conexao.cursor()
+    cursor.execute("SELECT SUM(valor_pedido) FROM comandas WHERE data_registro = CURRENT_DATE")
+    resultado = cursor.fetchone()[0]
+    total_acumulado_do_dia = resultado if resultado is not None else 0
+    faturamento_atual = 40 + total_acumulado_do_dia
+    historico_entregas = {}
+    # Criamos um dicionário vazio apenas para travar comandas repetidas na mesma sessão
+    historico_entregas = {}
+    cursor.execute("""
+CREATE TABLE IF NOT EXISTS comandas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    data_registro TEXT  DEFAULT CURRENT_DATE,
+    numero_comanda TEXT,
+    valor_pedido REAL
+)
+""")
     while True :
         comanda = str(input("Qual o Numero da Comanda ? (Ou digite 'sair' para fechar)"))
         if comanda.lower() == 'sair' :
@@ -14,14 +31,12 @@ def faturamento_diario():
             continue
         valores = int(input("Qual o valor do Pedido ?"))
         historico_entregas[comanda] = valores
+        faturamento_atual += valores
+        print(f"Estamos em {faturamento_atual}R$, Continua garoto!")
+        # 1. Preparar a ordem de inserção de dados
+        cursor.execute("INSERT INTO comandas (numero_comanda, valor_pedido) VALUES (?, ?)", (comanda, valores))
 
-        total = sum(historico_entregas.values())
-
-        print(f"Estamos em {total}R$, Continua garoto!")
-        with open("comandas.txt", 'a') as arquivo :
-            linha_texto = (f"Comanda {comanda}: Valor {valores}\n")
-            arquivo.write(linha_texto)
-
+        conexao.commit()
 
 if __name__ == '__main__':
     faturamento_diario()
